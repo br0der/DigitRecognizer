e847d77679979c5953f1249d91829e6f3b1cba1f
baber <aber.brady@gmail.com>
2024-10-22 15:44:06 -0400

    Separated code into backprop function and getError function. Made sample network to test

--- main.py
+++ main.py
@@ -7,7 +7,26 @@ mndata = MNIST('samples')
 
 images, labels = mndata.load_training()
 
-net = Network(28*28, 1, 16, 10)
+# net = Network(28*28, 1, 16, 10)
+w1 = [[1, 1, 0, 0], [0, 0, 1, 1]]
+w2 = [[1, -1], [-1, 1]]
+b2 = [0 for i in range(2)]
+b3 = [0 for i in range(2)]
+testNet = Network(4, 1, 2, 2, [w1, w2], [b2, b3])
 # net.setSigmoid()
 
-net.train(images, labels)
+# net.train()
+testImages = [[1, 0.5, 0, 1]]
+testLablels = [1]
+
+
+print(testNet.getError(testImages[0], testLablels[0]))
+print(testNet.weights)
+print(testNet.biases)
+
+print()
+
+testNet.train(testImages, testLablels)
+print(testNet.getError(testImages[0], testLablels[0]))
+print(testNet.weights)
+print(testNet.biases)
--- network.py
+++ network.py
@@ -50,7 +50,47 @@ class Network:
         self.derivatives = []
 
         self.derivativeStep = 0.0001
+
+        self.learningRate = 0.001
     
+    def __init__(self, inputDim, hiddenLayers, nodesPerLayer, outputDim, weights, biases):
+        assert(inputDim >= 1)
+        self.inputDim = inputDim
+
+        assert(hiddenLayers >= 1)
+        self.hiddenLayers = hiddenLayers
+
+        assert(nodesPerLayer >= 1)
+        self.nodesPerLayer = nodesPerLayer
+
+        assert(outputDim >= 1)
+        self.outputDim = outputDim
+
+        self.activation = lambda x: x if x >= 0 else 0
+        
+        self.weights = weights
+        # self.weights[0] = [[(random.random()*2) - 1 for j in range(inputDim)]
+        #                     for i in range(nodesPerLayer)]
+        # for i in range(1, hiddenLayers+1):
+        #     mtx = [[(random.random()*2) - 1 for j in range(nodesPerLayer)] 
+        #            for i in range(nodesPerLayer)]
+        #     self.weights[i] = mtx
+        # self.weights[-1] = [[(random.random()*2) - 1 for j in range(nodesPerLayer)]
+        #                     for i in range(outputDim)]
+
+        self.biases = biases
+        # for i in range(hiddenLayers):
+        #     mtx = [(random.random()*2) - 1 
+        #            for i in range(nodesPerLayer)]
+        #     self.biases[i] = mtx
+        # self.biases[-1] = [(random.random()*2) - 1
+        #                     for i in range(outputDim)]
+        self.derivatives = []
+
+        self.derivativeStep = 0.0001
+
+        self.learningRate = 0.01
+
     def setActivation(self, f):
         self.activation = f
 
@@ -73,7 +113,7 @@ class Network:
         return self.weights[layer][en][st]
 
     def getBias(self, layer, node):
-        return self.biases[layer][node]
+        return self.biases[layer-1][node]
     
     def nodesInLayer(self, layer):
         assert(0<=layer<=self.hiddenLayers+1)
@@ -84,24 +124,22 @@ class Network:
         return self.nodesPerLayer
         
     def scaleOutput(self, output):
-        assert(len(output)==10)
+        assert(len(output)==self.outputDim)
         mx_logit = max(output)
 
-        l = [math.pow(math.e, (item/mx_logit)) for item in output]
+        if mx_logit==0: l = [1 for item in output]
+        else: l = [math.pow(math.e, (item/mx_logit)) for item in output]
+
         s = sum(l)
         return [item/s for item in l]
 
     def forwardPropogate(self, picture):
+        # TODO: Keep track of all activations to use in back prop. Weight error is activation[left] * error[right]
         assert(len(picture) == self.inputDim)
 
         currLayer = picture
 
-        self.derivatives = [[] for i in range(self.hiddenLayers + 1)]
-        for layer in range(self.hiddenLayers+1):
-            self.derivatives[layer] = [\
-                [0.0 for leftNode in range(self.nodesInLayer(layer))] \
-                for rightNode in range(self.nodesInLayer(layer+1)) \
-            ]
+        self.derivatives = [[0.0 for j in range(self.nodesInLayer(i))] for i in range(self.hiddenLayers + 1)]
 
         # Pass it through each layer
         for layerIdx in range(0, self.hiddenLayers+1):
@@ -112,47 +150,81 @@ class Network:
             for rightNode in range(self.nodesInLayer(layerIdx+1)):
                 nextLayer[rightNode] += self.getBias(layerIdx+1, rightNode)
 
-                for leftNode in range(self.nodesInLayer(layerIdx)):
-                    self.derivatives[layerIdx][rightNode][leftNode] = \
-                        (self.activate(nextLayer[rightNode] + self.derivativeStep)
-                         - self.activate(nextLayer[rightNode] - self.derivativeStep)) \
-                        / (2*self.derivativeStep)
+                self.derivatives[layerIdx][rightNode] = \
+                    (self.activate(nextLayer[rightNode] + self.derivativeStep)
+                        - self.activate(nextLayer[rightNode] - self.derivativeStep)) \
+                    / (2*self.derivativeStep)
                     
                 nextLayer[rightNode] = self.activate(nextLayer[rightNode])
                 # print(nextLayer[j])
             currLayer = nextLayer
         
         output = currLayer
-        print(output)
+        # print(output)
         return self.scaleOutput(output)
 
-    def train(self, images, labels):
-        training = list(zip(images, labels))
-        random.shuffle(training)
-        ideal = [0.0 if i!=training[0][1] else 1.0 for i in range(10)]
-        output = self.forwardPropogate(training[0][0])
-        print(output)
-        
+    def getError(self, image, label):
+        ideal = [0.0 if i!=label else 1.0 for i in range(self.outputDim)]
+        output = self.forwardPropogate(image)
+        print(f"output: {output}")
         error = [-(ideal[i]*(math.log(output[i])) + (1-ideal[i])*(math.log(1-output[i]))) for i in range(self.outputDim)]
+        return error
+
+    def backPropogate(self, image, label):
+        error = self.getError(image, label)
         weightError = [[] for i in range(self.hiddenLayers + 1)]
         for layer in range(self.hiddenLayers+1):
             weightError[layer] = [\
                 [0.0 for leftNode in range(self.nodesInLayer(layer))] \
                 for rightNode in range(self.nodesInLayer(layer+1)) \
             ]
+        
+        biasError = [[0.0 for j in range(self.nodesInLayer(i+1))] for i in range(self.hiddenLayers + 1)]
 
-        print(output)
-        print(ideal)
-        print(error)
+        # print()
+        # print(output)
+        # print(ideal)
+        # print(error)
         for layer in reversed(range(self.hiddenLayers+1)):
+            for node in range(len(error)):
+                biasError[layer][node] = error[node]
             nextError = [0.0]*self.nodesInLayer(layer)
             for leftNode in range(self.nodesInLayer(layer)):
                 for rightNode in range(len(error)):
-                    nextError[leftNode] += error[rightNode] * self.derivatives[layer][rightNode][leftNode] * self.getWeight(layer, leftNode, rightNode)
-                    weightError[layer][rightNode][leftNode] = error[rightNode] * self.derivatives[layer][rightNode][leftNode]
+                    nextError[leftNode] += error[rightNode] * self.derivatives[layer][rightNode] * self.getWeight(layer, leftNode, rightNode)
+                    weightError[layer][rightNode][leftNode] = error[rightNode]
             error = nextError
+            
+        allErrors = []
+
         for layer in range(self.hiddenLayers+1):
             for rightNode in range(self.nodesInLayer(layer+1)):
-                print(weightError[layer][rightNode])
-            print()
-        # print(error)
+                # print(layer, rightNode, weightError[layer][rightNode])
+                for leftNode in range(self.nodesInLayer(layer)):
+                    allErrors.append(((layer, rightNode, leftNode), weightError[layer][rightNode][leftNode]))
+            # print()
+
+        for layer in range(self.hiddenLayers+1):
+            # print(layer, biasError[layer])
+            for node in range(self.nodesInLayer(layer+1)):
+                allErrors.append(((layer, node), biasError[layer][node]))
+        # print()
+
+        return allErrors
+
+    def train(self, images, labels):
+        training = list(zip(images, labels))
+        random.shuffle(training)
+        allErrors = self.backPropogate(training[0][0], training[0][1])
+        print(f"errors: {allErrors}")
+        for key, value in allErrors:
+            if len(key)==3:
+                layer, rightNode, leftNode = key
+                self.weights[layer][rightNode][leftNode] -= self.learningRate * value
+            elif len(key)==2:
+                layer, node = key
+                self.biases[layer][node] -= self.learningRate * value
+            else:
+                raise KeyError
+
+        
